f935c700479480a8fccd9e2c8d1cf228b04439b9
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-10-16 16:11:51 +0200

    Add --version option

--- src/hypermodern_python_project/__main__.py
+++ src/hypermodern_python_project/__main__.py
@@ -1,6 +1,9 @@
 import click
 
+from hypermodern_python_project import __version__
+
 
 @click.command()
+@click.version_option(version=__version__)
 def main():
     """The hypermodern Python script."""
