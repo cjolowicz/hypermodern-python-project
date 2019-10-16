386aa943bbb5277318567c6967e96c7113d362a4
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-10-16 11:29:21 +0200

    Add noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,9 @@
+import nox
+
+
+@nox.session(python="3.8")
+def tests(session):
+    """Run the test suite."""
+    env = {"VIRTUAL_ENV": session.virtualenv.location}
+    session.run("poetry", "install", external=True, env=env)
+    session.run("pytest", "--cov", *session.posargs)
