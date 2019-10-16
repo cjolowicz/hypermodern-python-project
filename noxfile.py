5cc6b3557ea9ad05c06f88b2293366b65826e78a
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-10-16 13:14:05 +0200

    Add lint session to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,13 @@ import nox
 
 
 @nox.session(python=["3.8", "3.7"])
+def lint(session):
+    """Lint using flake8."""
+    session.install("flake8", "flake8-bugbear", "flake8-import-order")
+    session.run("flake8", "src", "tests", "noxfile.py")
+
+
+@nox.session(python=["3.8", "3.7"])
 def tests(session):
     """Run the test suite."""
     env = {"VIRTUAL_ENV": session.virtualenv.location}
