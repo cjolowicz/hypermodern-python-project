71a0b794df043bdc41d2c017ab78b57e6855c3fb
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-10-22 22:32:44 +0200

    Add greeting

--- src/hypermodern_python_project/console.py
+++ src/hypermodern_python_project/console.py
@@ -4,6 +4,12 @@ from . import __version__
 
 
 @click.command()
+@click.argument("name", default="world")
+@click.option("--color/--no-color", default=True, help="Disable colors.")
 @click.version_option(version=__version__)
-def main():
-    """The hypermodern Python project."""
+def main(name, color):
+    """The hypermodern Python script."""
+    if color:
+        click.secho(f"Hello {name}", fg="green")
+    else:
+        click.echo(f"Hello {name}")
