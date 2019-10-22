351c5b121cb41241b4a758ba746e2ed805f106f0
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-10-22 22:18:11 +0200

    Add Sphinx configuration file

---
+++ docs/conf.py
@@ -0,0 +1,7 @@
+import sphinx_rtd_theme
+
+project = "hypermodern-python-project"
+author = "Claudio Jolowicz"
+copyright = f"2019, {author}"
+extensions = ["sphinx_rtd_theme"]
+html_theme = "sphinx_rtd_theme"
